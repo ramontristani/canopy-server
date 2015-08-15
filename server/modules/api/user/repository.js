c6213e6c42e3b8f28cf1c5965b76b56de0547548
Ramon E. Tristani <ramon.tristani@pcsweblabs.com>
2015-08-14 18:50:16 -0700

    Completed Default Client Account Creation
    Added logic to the user repository to create default client accounts on
    the DiskDB collection.

--- server/modules/api/user/repository.js
+++ server/modules/api/user/repository.js
@@ -1,33 +1,72 @@
 'use strict';
 
-var User = require('../../schemas').User
-	, clientAccount = require('../../../../startup/config').accounts.client;
+var bcrypt = require('bcrypt')
+	, config = require('../../../../startup/config')
+	, clientAccount = config.accounts.client;
 
 module.exports = {
-	createDefaultClientAccount: function(done) {
-		User.findOne({ email: clientAccount.email }, function(error, user) {
-			if (error) {
-				return done(error);	
-			}
+	createDefaultClientAccount: function(diskdb, done) {
+		if (!diskdb) {
+			var User = require('../../schemas').User;
 			
-			if (user) {
-				return done(null, {
-					message: '- Reusing existing client account: ',
-					document: user
-				});
-			}
-			
-			var client = new User(clientAccount);
-			client.save(function(error, doc) {
+			User.findOne({ email: clientAccount.email }, function(error, user) {
 				if (error) {
 					return done(error);	
 				}
 				
-				done(null, {
-					message: '- Successfully created client user account: ',
-					document: doc
+				if (user) {
+					return done(null, {
+						message: '- Reusing existing client account: ',
+						document: user
+					});
+				}
+				
+				var client = new User(clientAccount);
+				client.save(function(error, doc) {
+					if (error) {
+						return done(error);	
+					}
+					
+					done(null, {
+						message: '- Successfully created client user account: ',
+						document: doc
+					});
 				});
 			});
-		});
+		} else {
+			try {
+				diskdb.loadCollections(['users']);
+				var user = diskdb.users.findOne({ email: clientAccount.email });
+				
+				if (user) {
+					return done(null, {
+						message: '- Reusing existing client account: ',
+						document: user
+					});
+				} else {
+					bcrypt.genSalt(config.crypto.swf, function(error, salt) {
+						if (error) {
+							throw error;
+						}
+						
+						bcrypt.hash(clientAccount.secret, salt, function(error, hash) {
+							if (error) {
+								throw error;
+							}
+							
+							clientAccount.secret = hash;
+							user = diskdb.users.save(clientAccount);
+							done(null, {
+								message: '- Successfully created client user account: ',
+								document: user
+							});
+						});
+					});
+				}
+				
+			} catch (e) {
+				return done(e);
+			}
+		}
 	} 
 };
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 'use strict';
-var config = require('./config');
+var userRepository = require('../server/modules/api/user/repository')
+	, config = require('./config');
 
 console.log('- Initializing database connection');
 module.exports = function() {
-	var db;
+	var errorMessage = '- **** Error creating default client account ****'
+		, db;
 	
 	if (config.database.diskdb.setdefault) {
 		var diskdb = require('diskdb')
@@ -11,18 +13,24 @@ module.exports = function() {
 			
 		db = diskdb.connect(path, config.database.diskdb.collections);
 		console.log('- Successfully connected to DiskDB: ' + path);
+		
+		userRepository.createDefaultClientAccount(db, function(error, result) {
+			console.log(!error && result
+				? result.message + result.document.email
+				: errorMessage);
+		});
+		
 	} else if (config.database.mongodb.setdefault) {
 		var mongoose = require('mongoose');
 		db = mongoose.connect(config.database.mongodb.connection);
 		console.log('- Successfully connected to MongoDB: ' + config.database.mongodb.connection);
 		
 		console.log('- Creating default client account...');
-		var userRepository = require('../server/modules/api/user/repository');
 		
-		userRepository.createDefaultClientAccount(function(error, result) {
+		userRepository.createDefaultClientAccount(null, function(error, result) {
 			console.log(!error && result
 				? result.message + result.document.email
-				: '- **** Error creating default client account ****');
+				: errorMessage);
 		});
 	}
 	
