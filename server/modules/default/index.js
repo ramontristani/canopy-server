fabcff380cdb6dbb0b84a947310101766212da52
Ramon Tristani <moncho@Ramons-MacBook-Pro.local>
2015-08-24 12:24:19 -0700

    Automatic Vendor Scripts
    Removed vendor script references from assets.js to keep file small and
    automatically pull in bower scripts when a given rout is invoked. There
    is no need now to define bower script files in the assets.js file, just
    custom scripts

--- server/modules/default/index.js
+++ server/modules/default/index.js
@@ -1,20 +1,59 @@
 'use strict';
 
-var _ = require('lodash');
+var _ = require('lodash')
+	, bowerMains = require('main-bower-files');
+
+var vendorScripts = function (config) {
+	var localVendorPaths = bowerMains('**/*.js')
+		, result = [];
+
+	_.each(localVendorPaths, function (script) {
+		var start = script.indexOf('/vendor')
+			, end = script.length;
+
+		result.push(script.slice(start, end));
+	});
+
+	if (config.server.socketsenabled) {
+		result.unshift('/socket.io/socket.io.js');
+	}
+
+	return result;
+};
+
+var customScripts = function (assets) {
+	return _.pluck(_.filter(assets.js, function (asset) {
+		return asset.ownerpartial === 'index'
+			|| asset.ownerpartial === 'all'
+	}), 'path');
+};
+
+var aggregateScripts = function (vendor, custom) {
+	var result = vendor;
+	_.each(custom, function (path) {
+		result.push(path);
+	});
+
+	return result;
+};
+
+var cssSources = function (assets) {
+	return _.pluck(_.filter(assets.css, function (asset) {
+		return asset.ownerpartial === 'index'
+			|| asset.ownerpartial === 'all'
+	}), 'path');
+};
 
 module.exports = {
-	register: function(server) {
-		server.app.route('/').get(function(request, response) {
-			var css = _.pluck(_.filter(server.assets.css, function(asset) {
-					return asset.ownerpartial === 'index' 
-						|| asset.ownerpartial === 'all' 
-				}), 'path');
-				
-			var js = _.pluck(_.filter(server.assets.js, function(asset) {
-					return asset.ownerpartial === 'index' 
-						|| asset.ownerpartial === 'all' 
-				}), 'path');
-				
+	register: function (server) {
+		server.app.route('/').get(function (request, response) {
+			var js = aggregateScripts(vendorScripts(server.config), customScripts(server.assets))
+				, css = cssSources(server.assets);
+
+			_.each(customScripts, function (path) {
+				js.push(path);
+			});
+
 			var options = {
 				title: server.config.general.title,
 				description: server.config.general.description,
@@ -22,7 +61,7 @@ module.exports = {
 				css: css,
 				js: js
 			};
-			
+
 			response.render('index', options);
 		});
 	}
--- startup/assets.js
+++ startup/assets.js
@@ -10,23 +10,14 @@ var assets = function() {
 			{ path: '/styles/default.css', ownerpartial: 'index' }
 		],
 		js: [
-			{ path: '/vendor/lodash/lodash.min.js', ownerpartial: 'all' },		
-			{ path: '/vendor/jquery/dist/jquery.min.js', ownerpartial: 'all' },		
-			{ path: '/vendor/bootstrap/dist/js/bootstrap.min.js', ownerpartial: 'all' },		
-			{ path: '/vendor/angular/angular.js', ownerpartial: 'all' },		
-			{ path: '/vendor/angular-route/angular-route.js', ownerpartial: 'all' },
-			{ path: '/modules/constants/constants.js', ownerpartial: 'all' },		
 			{ path: '/modules/main.js', ownerpartial: 'index' },		
+			{ path: '/modules/constants/constants.js', ownerpartial: 'all' },
 			{ path: '/modules/layout/layout.js', ownerpartial: 'index' },
 			{ path: '/modules/home/home.js', ownerpartial: 'index' },
 			{ path: '/modules/about/about.js', ownerpartial: 'index' },
 			{ path: '/modules/authentication/authentication.js', ownerpartial: 'all' }
 		]
 	};
-	
-	if (config.server.socketsenabled) {
-		result.js.unshift({ path: '/socket.io/socket.io.js', ownerpartial: 'all' });
-	}
 	
 	return result;
 };
