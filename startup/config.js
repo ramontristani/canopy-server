d244660a12307676c1e9a300d429d977419e1f82
Ramon E. Tristani <ramon.tristani@pcsweblabs.com>
2015-09-01 10:50:27 -0700

    Removed Comments
    Removed docker comments. Seems I had more documentation than actual
    code.
    
    Added ".settings/" exception to gitignore file to prevent VSCode
    settings files from getting added to the repository

--- startup/config.js
+++ startup/config.js
@@ -1,76 +1,21 @@
-/**
-* #DISCLAIMER
-* Passwords must be changed and stored securely. All accounts,  acount as defined
-* in this file should be modified to suit your needs. Sufice to say, this file
-* contains sensitive information and should not be added to publically viewable
-* source control services such as Bitbucket or GitHub. Please store in a private
-* repository.
-*/
 'use strict';
 
 console.log('\n- Initializing server configuration');
-
-/**
- * ##Main Configuration Module
- * The main configuration module defines all of the operational settings and characteristics of the application server. <strong>NOTE: </strong>This file should be kept secure
- * @general Singleton container of application descriptive attributes
- * @server Singleton container of server specific options resolved at runtime
- * @development Singleton container of environmental options specific for development
- * @keys Singleton container used for holding string keys
- * @tokens Singleton container used JSON web token configuration options
- * @crypto Singleton container used for cryptography options
- * @accounts Singleton container used for storing default accounts that should be created on first run
- * @database Singleton container used for defining database server options
- * @mail Singleton container used for storing mail configuration options
- * @exports Configuration singleton object
- */
 module.exports = {
-	/**
-	 * ##General Configuration Options
-	 * The general configuration options provide a container of non operational descriptive attributes used by the application server
-	 * @title String property representing the title of the application
-	 * @description String property representing the application description
-	 */
 	general: {
 		title: 'Canopy Server',
 		description: 'Basic web application server for sites or RESTful API\'s'
 	},
 	
-	/**
-	 * ##Server Configuration Options
-	 * The server configuration options define a container of settings used for defining how the server behaves ar runtime
-	 * @port Port assigned to the server
-	 * @deploymenttype Defines whether the server is deployd in "dev" or "prod" mode
-	 * @enablecors Enables/Disables cross-domain service enpoint requests
-	 * @logroutes Enables/Disables logging of route requests to the database
-	 * @socketsenabled Enables/Disables web sockets with socket.io
-	 * @minification Enables/Disables minified HTML rendered output from server views
-	 * @see https://www.npmjs.com/package/socket.io
-	 */
 	server: {
 		port: process.env.PORT || 1337,
 		deploymenttype: 'dev',
 		enablecors: true,
 		logroutes: true,
 		socketsenabled: true,
-		/**
-		 * ###Minification options
-		 * @minifyrenders Enables/Disables minified rendered HTML from the server
-		 * @options Minification options as required by the "html-minifier" NPM package
-		 * @see https://www.npmjs.com/package/html-minifier
-		 */
+		
 		minification: {
 			minifyrenders: true,
-			/**
-			 * ###HTML ninifier parameters
-			 * @removeComments
-			 * @removeCommentsFromCDATA
-			 * @collapseWhitespace
-			 * @collapseBooleanAttributes
-			 * @removeAttributeQuotes
-			 * @removeEmptyAttributes
-			 * @see https://www.npmjs.com/package/html-minifier
-			 */
 			options: {
 					removeComments: true,
 					removeCommentsFromCDATA: true,
@@ -82,41 +27,17 @@ module.exports = {
 		}
 	},
 	
-	/**
-	 * ##Development Options
-	 * The development oftions section provides a container that facilitates the definition of operational characteristics during development
-	 * @openbrowser Enables/Disables automatic loading of the root route on the web browser on launch
-	 * @location Default URL to be opened by the browser on launch
-	 * @browser When left empty, the default browser is used. Specify other browsers for other options, i.e. "Safari" or "Firefox"
-	 * @see https://www.npmjs.com/package/open
-	 */
 	development: {
 		openbrowser: false,
 		location: 'http://localhost:1337',
 		browser: ''
 	},
 	
-	/**
-	 * ##Encoding Keys
-	 * The encoding keys options provides a container that stores special keys for general encoding/decoding operations
-	 * @sessions Key used for encoding session data
-	 * @cookies Key used for encoding cookie data
-	 */
 	keys: {
 		sessions: '0c67f367-cc77-45fd-a2ec-2ea3a6f51ba8',
 		cookies: '8df275b7-f35f-4ea1-bcc8-052817d780ad'
 	},
 	
-	/**
-	 * ##JSON Web Token Options
-	 * The JSON web token options provide a boilerplate definition of data that will be used for encoding JSON web tokens
-	 * @issuer Name of the entity issuing the JSON web token
-	 * @subject A description of the reason for issuing the web token
-	 * @secret A string key used for encoding the JSON web token
-	 * @daysexpires A date value used for setting an expiration limit on the JSON web token
-	 * @jwtencoding Encoding algorithm used for encoding the JSON web token
-	 * @see https://www.npmjs.com/package/jwt-simple
-	 */
 	tokens: {
 		issuer: 'localhost@canopyserver.com',
 		subject: 'API Access',
@@ -125,22 +46,10 @@ module.exports = {
 		jwtencoding: 'HS512'
 	},
 	
-	/**
-	 * ##Encryption options
-	 * The encryption options allows for the definition of cryptography operation options such as those used for hashing account passwords
-	 * @swf Salt work factor value
-	 * @see https://www.npmjs.com/package/bcrypt
-	 */
 	crypto: {
 		swf: 10
 	},
 	
-	/**
-	 * ##Default System Accounts
-	 * The default system accounts configuration allows the application to create default accounts for services, or administration purposes.
-	 * @maxloginattempts Maximum number of failed login attempts prior to accout locking. This helps prevent brute force attacks
-	 * @objects Account object array containing default account definitions
-	 */
 	accounts: {
 		maxloginattempts: 5,
 		objects: [
@@ -157,32 +66,12 @@ module.exports = {
 		]
 	},
 	
-	/**
-	 * ## Database Options
-	 * The database configuration options are used for defining the database engine to be used by the
-	 * application server. Database providers are mutually exclusive. Pick one or the other!
-	 * @mongodb MongoDB connection options
-	 * @diskdb DiskDB connection options
-	 */
 	database: {
-		/**
-		 * ###Mongoose options
-		 * @setdefault Sets MongoDB as the default database. Set "diskdb.setdefault" to false when using MongoDB
-		 * @connection The connection string to the server/database
-		 * @see https://www.npmjs.com/package/mongoose
-		 */
 		mongodb: {
 			setdefault: true,
 			connection: 'mongodb://localhost/canopy-server'
 		},
 		
-		/**
-		 * ###DiskDB Options
-		 * @setdefault Sets DiskDB as the default database. Set "mongodb.setdefault" to false when using DiskDB
-		 * @path The physical folder path where DiskDB will store its data files. Default is "./data"
-		 * @collections The default collections that should be created in the file system on first run
-		 * @see https://www.npmjs.com/package/diskdb
-		 */
 		diskdb: {
 			setdefault: false,
 			path: 'data',
@@ -193,34 +82,10 @@ module.exports = {
 		}
 	},
 	
-	/**
-	 * ##Mail Configuration Options
-	 * The mail configuration options are used for specifying the default service and account used for mail 
-	 * sending operations by the application server
-	 * @service Name of the mail service used for SMTP operations
-	 * @auth Mail service credentials object
-	 * @see https://www.npmjs.com/package/nodemailer
-	 */
 	mail: {
-		/**
-		 * ##Enable
-		 * Set to true if the application will have mail sending capabilities. The SMTP transport will 
-		 * then be attached to the configured server object. If mail is enabled without a provider, 
-		 * mail will still work but most email clients will route the email to the junk folder.
-		 */
 		enable: true,
 		settings: {
-			/**
-			* ##Mail Service Provider
-			* The mail service provider from which to create the SMTP transport
-			*/
 			service: '',
-			
-			/**
-			* ###Mail Authentication Options
-			* @user User name used for logging in to the mail service (usually and email address)
-			* @pwd Password used in combilation with a user name to log in to the mail service
-			*/
 			auth: {
 				user: '',
 				pwd: ''
--- startup/index.js
+++ startup/index.js
@@ -12,41 +12,17 @@ var config = require('./config')
 	, http = require('http')
 	, io = require('socket.io');
 
-/**
- * Application startup module
- * @exports	An application startup singleton
- */
 module.exports = {
-	/**
-	 * Server function returns a decorated application server singleton
-	 * @param done: Callback invoked when all initialization operations have been completed
-	 * @return A server singleton object via callback
-	 */
 	server: function(done) {
-		/**
-		 * Database confuguration module is executed to get a reference to DiskDB or MongoDB
-		 * @param callback:	Returns a database reference
-		 */		
 		db(function(error, database) {
 			if (error) {
 				return console.log(error);
 			}
 			
-			/**
-			* Creating express and http instances
-			*/
 			var app = express(database)
 				, server = http.Server(app)
 				, transport = mail();
 			
-			/**
-			* Constructed application server singleton
-			* @property app: References an ExpressJS instance
-			* @property http: References the http server created from the ExpressJS instance
-			* @property db: References the configured application database connection
-			* @property config: Contains a reference to the main "startup/config.js" file
-			* @property assets: Contains a reference to the main "startup/assets.js" file
-			*/
 			var result = {
 				app: app,
 				http: server,
@@ -56,17 +32,10 @@ module.exports = {
 				mailTransport: transport
 			};
 			
-			/**
-			* Enables web sockets when enabled in main config file
-			*/
 			if (config.server.socketsenabled) {
 				result.io = io(server);
 			}
 			
-			/**
-			* Executes callback function by returning server singleton to the caller
-			* @param result: A configured server singleton
-			*/
 			done(null, result);
 		});
 	}
